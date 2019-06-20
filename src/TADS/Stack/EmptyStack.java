d99ca44d82c84c323de8be7eabdca571e5ba8f33
Joaquin Martinez <joaco1509@gmail.com>
2019-06-19 22:12:23 -0300

    Eliminando tads no utilizados

--- src/TADS/Stack/EmptyStack.java
+++
@@ -1,9 +0,0 @@
-package TADS.Stack;
-
-public class EmptyStack extends Exception {
-    public static final long serialVersionUID=700L;
-
-    public EmptyStack(String mensaje){
-        super(mensaje);
-    }
-}
--- src/TADS/Stack/MyStack.java
+++
@@ -1,8 +0,0 @@
-package TADS.Stack;
-public interface MyStack <T>{
-    void pop () throws  EmptyStack;
-    T top() throws EmptyStack;
-    void push(Nodo<T> element) throws  EmptyStack;
-    boolean isEmpty ();
-    void makeEmpty() throws  EmptyStack;
-}
